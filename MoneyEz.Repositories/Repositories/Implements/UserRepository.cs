6d2f3801cad8c96e47cd4ac74e3134b8d96cc835
Dang Gia Duc <giaducdang@gmail.com>
2025-01-21 20:42:39 +0700

    fix: authen, user api

--- MoneyEz.Repositories/Repositories/Implements/UserRepository.cs
+++ MoneyEz.Repositories/Repositories/Implements/UserRepository.cs
@@ -1,26 +1,26 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using MoneyEz.Repositories.Entities;
-//using MoneyEz.Repositories.Repositories.Interfaces;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyEz.Repositories.Entities;
+using MoneyEz.Repositories.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace MoneyEz.Repositories.Repositories.Implements
-//{
-//    public class UserRepository : GenericRepository<User>, IUserRepository
-//    {
-//        private readonly MoneyEzContext _context;
+namespace MoneyEz.Repositories.Repositories.Implements
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        private readonly MoneyEzContext _context;
 
-//        public UserRepository(MoneyEzContext context) : base(context)
-//        {
-//            _context = context;
-//        }
+        public UserRepository(MoneyEzContext context) : base(context)
+        {
+            _context = context;
+        }
 
-//        public async Task<User> GetUserByEmailAsync(string email)
-//        {
-//            return await _context.Users.FirstOrDefaultAsync(e => e.Email == email);
-//        }
-//    }
-//}
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(e => e.Email == email);
+        }
+    }
+}
