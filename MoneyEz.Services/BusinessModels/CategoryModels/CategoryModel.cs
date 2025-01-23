d3cb9d5b7dc398d09a9c190757571aa4ce866b46
Nguyễn Thương Huyền <thuonghuyennth.8499@gmail.com>
2025-01-22 19:53:14 -0500

    fix(category-model): extend baseentity in categorymodel to relief duplicate in baseentity

--- MoneyEz.Services/BusinessModels/CategoryModels/CategoryModel.cs
+++ MoneyEz.Services/BusinessModels/CategoryModels/CategoryModel.cs
@@ -1,19 +1,14 @@
-﻿namespace MoneyEz.Services.BusinessModels.CategoryModels
+﻿using MoneyEz.Repositories.Entities;
+
+namespace MoneyEz.Services.BusinessModels.CategoryModels
 {
-    public class CategoryModel
+    public class CategoryModel:BaseEntity
     {
         public Guid Id { get; set; } // ID danh mục
         public string Name { get; set; } // Tên danh mục
         public string NameUnsign { get; set; } // Tên không dấu
         public Guid? ModelId { get; set; } // ID mô hình liên quan
         public string Description { get; set; } // Mô tả danh mục
-
-        // Thuộc tính từ BaseEntity
-        public DateTime CreatedDate { get; set; } // Ngày tạo
-        public string CreatedBy { get; set; } // Người tạo
-        public DateTime? UpdatedDate { get; set; } // Ngày cập nhật
-        public string UpdatedBy { get; set; } // Người cập nhật
-        public bool IsDeleted { get; set; } // Trạng thái xóa mềm
 
         
       /*  public List<SubcategoryModel> Subcategories { get; set; } = new List<SubcategoryModel>();
