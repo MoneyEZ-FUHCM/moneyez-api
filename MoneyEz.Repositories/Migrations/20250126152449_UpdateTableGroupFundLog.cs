5cd3263b5993cb913745b28466c7ee2430d0004e
bomismegg <minhtri28102003@gmail.com>
2025-01-26 22:27:37 +0700

    fix(database): update table GroupFundLog

---
+++ MoneyEz.Repositories/Migrations/20250126152449_UpdateTableGroupFundLog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyEz.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableGroupFundLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Action",
+                table: "GroupFundLog",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Action",
+                table: "GroupFundLog");
+        }
+    }
+}
