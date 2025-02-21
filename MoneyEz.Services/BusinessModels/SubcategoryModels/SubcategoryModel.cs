342c55b0825e3c8e9ba0cb0fddb2d3156fdb3063
Nguyễn Thương Huyền <83652727+ngthuonghuyenn@users.noreply.github.com>
2025-02-21 12:02:44 +0700

    fix(category): remove repeating mapper, remove assign subcategory in update category, update BO up to n-n relationship (#128)
    * fix: change relation between Category and Subcategory
    
    * fix(entity): update table CategorySubcategory
    
    * fix(subcategory-management): add authorization
    
    * fix(subcategory): update BO up to n-n relationship
    
    * fix(subcategory): remove assign subcategory in update category, fix assign sub to cate
    
    * fix(category-subcategory): remove repeating mapper from category and subcategory
    
    ---------
    
    Co-authored-by: bomismegg <minhtri28102003@gmail.com>
    Co-authored-by: Dang Gia Duc <giaducdang@gmail.com>

--- MoneyEz.Services/BusinessModels/SubcategoryModels/SubcategoryModel.cs
+++ MoneyEz.Services/BusinessModels/SubcategoryModels/SubcategoryModel.cs
@@ -2,9 +2,6 @@
 using MoneyEz.Services.BusinessModels.CategoryModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoneyEz.Services.BusinessModels.SubcategoryModels
 {
@@ -13,7 +10,5 @@ namespace MoneyEz.Services.BusinessModels.SubcategoryModels
         public required string Name { get; set; }
         public required string NameUnsign { get; set; }
         public required string Description { get; set; }
-
-        public List<CategoryModel> Categories { get; set; } = new List<CategoryModel>();
     }
 }
