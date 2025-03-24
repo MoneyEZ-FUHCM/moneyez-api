cc592262b25a56c3e4f9bfa8186e5e6a05cbd4a1
Dang Gia Duc <giaducdang@gmail.com>
2025-03-25 01:13:42 +0700

    feat (group): get group logs

---
+++ MoneyEz.Services/Mappers/GroupMemLogConfig.cs
@@ -0,0 +1,25 @@
+﻿using MoneyEz.Repositories.Commons;
+using MoneyEz.Repositories.Entities;
+using MoneyEz.Services.BusinessModels.GroupMemLogModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyEz.Services.Mappers
+{
+    public partial class MapperConfig
+    {
+        partial void GroupMemLogConfig()
+        {
+            CreateMap<GroupMemberLog, GroupMemLogModel>();
+            CreateMap<GroupMemLogModel, GroupMemberLog>();
+
+            // Sử dụng PaginationConverter để ánh xạ Pagination<GroupMemberLog> sang Pagination<GroupMemLogModel>
+            CreateMap<Pagination<GroupMemberLog>, Pagination<GroupMemLogModel>>()
+                .ConvertUsing<PaginationConverter<GroupMemberLog, GroupMemLogModel>>();
+
+        }
+    }
+}
