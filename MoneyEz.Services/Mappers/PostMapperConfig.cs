852ebe070ecd9fffbc563f36c4c363ee25bb63f4
Dang Gia Duc <giaducdang@gmail.com>
2025-04-13 10:14:28 +0700

    fix: post mapper config

---
+++ MoneyEz.Services/Mappers/PostMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MoneyEz.Repositories.Commons;
+using MoneyEz.Repositories.Entities;
+using MoneyEz.Services.BusinessModels.BankAccountModels;
+using MoneyEz.Services.BusinessModels.PostModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyEz.Services.Mappers
+{
+    public partial class MapperConfig : Profile
+    {
+        partial void PostMapperConfig() 
+        { 
+            CreateMap<CreatePostModel, Post>();
+
+            CreateMap<Post, PostModel>();
+
+            CreateMap<Pagination<Post>, Pagination<PostModel>>().ConvertUsing<PaginationConverter<Post, PostModel>>();
+        }
+    }
+}
